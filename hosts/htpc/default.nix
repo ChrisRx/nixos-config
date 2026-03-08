30ca1ba3a2fe24132db53caad7e1700a1b17ea69
Chris Marshall <chris@couch.life>
2026-03-08 12:28:39 -0400

    chore: rename warning(s)

--- hosts/htpc/default.nix
+++ hosts/htpc/default.nix
@@ -18,7 +18,7 @@
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
-  services.logind.lidSwitch = "ignore";
+  services.logind.settings = { Login = { HandleLidSwitch = "ignore"; }; };
 
   home-manager.users.${username}.packages.experimental.enable = true;
   users.users.${username}.extraGroups = [ "docker" ];
