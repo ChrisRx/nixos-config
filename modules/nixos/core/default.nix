6d71243beed9fc65243e1be5436b4e1f0371547f
Chris Marshall <chris@couch.life>
2026-03-09 08:55:33 -0400

    experiments with core module configuration

--- modules/nixos/core/default.nix
+++ modules/nixos/core/default.nix
@@ -1,4 +1,17 @@
-{ ... }: {
+{ config, lib, username, ... }:
+
+let cfg = config.core;
+in {
   imports =
     [ ./boot.nix ./networking.nix ./programs.nix ./system.nix ./user.nix ];
+  options.core = {
+    user = {
+      extraGroups = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        description = "";
+      };
+    };
+  };
+
+  config = { users.users.${username}.extraGroups = cfg.user.extraGroups; };
 }
