8785435a5379e795339ef3000dc5d0d60bc24d73
Chris Marshall <chris@couch.life>
2026-09-14 17:35:01 -0400

    fix(hosts/htpc): pin kernel version to 7.1.13
    The latest version of open nvidia drivers fails to compile with the
    7.2.x kernel so until it is addressed this pins the kernel for htpc to
    the last know working kernel.

--- hosts/htpc/default.nix
+++ hosts/htpc/default.nix
@@ -1,5 +1,19 @@
-{ username, lib, ... }: {
-  imports = [ ./hardware-configuration.nix ./../../modules/nixos/core ];
+{
+  username,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/nixos/core
+    ./../../modules/nixos/gnome
+    ./../../modules/nixos/nvidia
+    ./../../modules/nixos/steam
+  ];
+
+  core.kernel.version = "7.1.13";
 
   core.packages = {
     development.enable = true;
@@ -8,12 +22,18 @@
   };
   gnome.enable = true;
   networking.hostName = "htpc";
-  nvidia = {
-    enable = true;
-    powerLimit = 250;
-    usePatchedPackage = true;
-  };
+
+  nvidia.enable = true;
+  nvidia.powerLimit = 300;
+  nvidia.usePatchedPackage = false;
+  hardware.nvidia.nvidiaPersistenced = true;
+
   steam.enable = true;
+
+  home-manager.users.${username}.home.packages = with pkgs; [
+    pcsx2
+    azahar
+  ];
 
   systemd = {
     targets.sleep.enable = false;
@@ -21,28 +41,41 @@
     targets.hibernate.enable = false;
     targets.hybrid-sleep.enable = false;
   };
-  services.logind.settings = { Login = { HandleLidSwitch = "ignore"; }; };
+  services.logind.settings = {
+    Login = {
+      HandleLidSwitch = "ignore";
+    };
+  };
 
-  fileSystems = builtins.listToAttrs (builtins.map (share:
-    lib.nameValuePair "/mnt/${share}" {
-      device = "//storage.oldtech.internal/${share}";
-      fsType = "cifs";
-      options = [
-        (lib.strings.join "," [
-          "x-systemd.automount"
-          "noauto"
-          "x-systemd.idle-timeout=60"
-          "x-systemd.device-timeout=5s"
-          "x-systemd.mount-timeout=5s"
-          "credentials=/etc/nixos/smb-secrets"
-          "uid=${username}"
-          "gid=users"
-        ])
-      ];
-    }) [ "media" "public" ]);
+  fileSystems = builtins.listToAttrs (
+    map
+      (
+        share:
+        lib.nameValuePair "/mnt/${share}" {
+          device = "//storage.oldtech.internal/${share}";
+          fsType = "cifs";
+          options = [
+            (lib.strings.join "," [
+              "x-systemd.automount"
+              "noauto"
+              "x-systemd.idle-timeout=60"
+              "x-systemd.device-timeout=5s"
+              "x-systemd.mount-timeout=5s"
+              "credentials=/etc/nixos/smb-secrets"
+              "uid=${username}"
+              "gid=users"
+            ])
+          ];
+        }
+      )
+      [
+        "media"
+        "public"
+      ]
+  );
 
   # TODO: disable again when debug finished
-  services.openssh.enable = true;
+  services.openssh.enable = false;
 
   system.stateVersion = "25.11";
 }
--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -1,12 +1,18 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-let cfg = config.nvidia;
-in {
+let
+  cfg = config.nvidia;
+in
+{
   options.nvidia = {
     enable = lib.mkEnableOption "Enable nvidia gpu";
 
-    usePatchedPackage =
-      lib.mkEnableOption "Use NVidia package with 6.19 kernel patch";
+    usePatchedPackage = lib.mkEnableOption "Use NVidia package with 6.19 kernel patch";
 
     powerLimit = lib.mkOption {
       type = lib.types.int;
@@ -26,34 +32,36 @@ in {
     };
 
     # Apply CachyOS kernel 6.19 patch to NVIDIA latest driver (optional)
-    hardware.nvidia.package = lib.mkIf cfg.usePatchedPackage (let
-      base = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "590.48.01";
-        sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
-        openSha256 = "sha256-hECHfguzwduEfPo5pCDjWE/MjtRDhINVr4b1awFdP44=";
-        settingsSha256 = "sha256-4SfCWp3swUp+x+4cuIZ7SA5H7/NoizqgPJ6S9fm90fA=";
-        persistencedSha256 = "";
-      };
-      cachyos-nvidia-patch = pkgs.fetchpatch {
-        url =
-          "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
-        sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
-      };
+    hardware.nvidia.package = lib.mkIf cfg.usePatchedPackage (
+      let
+        base = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+          version = "590.48.01";
+          sha256_64bit = "sha256-ueL4BpN4FDHMh/TNKRCeEz3Oy1ClDWto1LO/LWlr1ok=";
+          openSha256 = "sha256-hECHfguzwduEfPo5pCDjWE/MjtRDhINVr4b1awFdP44=";
+          settingsSha256 = "sha256-4SfCWp3swUp+x+4cuIZ7SA5H7/NoizqgPJ6S9fm90fA=";
+          persistencedSha256 = "";
+        };
+        cachyos-nvidia-patch = pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+          sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+        };
 
-      # Patch the appropriate driver based on config.hardware.nvidia.open
-      driverAttr = if config.hardware.nvidia.open then "open" else "bin";
-    in base // {
-      ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
-        patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
-      });
-    });
+        # Patch the appropriate driver based on config.hardware.nvidia.open
+        driverAttr = if config.hardware.nvidia.open then "open" else "bin";
+      in
+      base
+      // {
+        ${driverAttr} = base.${driverAttr}.overrideAttrs (oldAttrs: {
+          patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+        });
+      }
+    );
 
     systemd.services.nvidia-power-limit = lib.mkIf (cfg.powerLimit != 0) {
       description = "Set the power limit for NVIDIA gpu";
       serviceConfig = {
         Type = "oneshot";
-        ExecStart =
-          "${pkgs.bash}/bin/bash -c '/run/current-system/sw/bin/nvidia-smi -pm 1 && /run/current-system/sw/bin/nvidia-smi -pl 250'";
+        ExecStart = "${pkgs.bash}/bin/bash -c '/run/current-system/sw/bin/nvidia-smi -pm 1 && /run/current-system/sw/bin/nvidia-smi -pl ${toString cfg.powerLimit}'";
       };
       wantedBy = [ "multi-user.target" ];
     };
